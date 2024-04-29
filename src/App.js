e595cff58f219b346702bb65a235afd1cca7f880
zoewang7512 <zoewang7512@gmail.com>
2024-04-29 15:16:19 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+//components
+import Footer from "./Footer";
+//assets
+import Zero from './assets/emergencyCall.jpg';
+import One from './assets/tooBad.jpg';
+import Two from './assets/lottery.jpg';
+import Three from './assets/goodNewsBadNews.jpg';
+import Four from './assets/404.jpg';
+import Five from './assets/notMe.jpg';
 
 function App() {
+   // get the random Integer number 0 -> 5
+const rand1 = Math.floor(Math.random() * 6);
+
+function loadComponent(number) {
+  switch (number) {
+    case 0:
+      return <Message text={"I will be D.D.D. from 13/10 - 17/10. If it's an emergency call 3-3-1-2"} image={Zero}/>;
+      case 1:
+        return <Message text={"If you need me ... too bad!"} image={One}/>;
+        case 2:
+          return <Message text={"On vacation. Hoping to win the lottery and never return."} image={Two}/>;
+          case 3:
+            return <Message text={"The bad news is that I'm out of office, the good news is that I'm out of office."} image={Three}/>;
+            case 4:
+              return <Message text={"ERROR 404: Employee not found"} image={Four}/>;
+              case 5:
+                return <Message text={"Contact someone other than me."} image={Five}/>;
+            
+      default:
+          return "default"
+  }
+}
+
+const Message =({text,image})=>{
+  return<div>
+  <p>{text}</p>
+  <img src={image} height={'300px'} loading="lazy"/>
+  </div>
+};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="Body">
+        <h1>Oops!</h1>
+        {loadComponent(rand1)}
+      </div>
+ <Footer/>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import Logo from "./assets/LoveMyJobLogo.jpg";
+
+const Footer =()=>{
+    return(<div className="Footer"><img src={Logo} width={'100px'}/>
+        <p>Copyright © LoveMyJob Website 2024</p>
+    <p><a href="https://www.instagram.com/reel/C536o_-RsPT/?igsh=MXhuZHB1OTBmZDJkYw==">All inspirations originate from🌟</a></p>
+    </div>);
+};
+export default Footer;
